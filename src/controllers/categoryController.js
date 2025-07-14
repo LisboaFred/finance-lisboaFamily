6c8c48e3fa8a7e6f7b767e8f43b4aacba1dabb7a
LisboaFred <contato.fredericolisboa@gmail.com>
2025-07-14 14:52:04 -0300

    feat: criação da src para futuro incremento de TS

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,4 +1,4 @@
-const Category = require('../models/Category');
+const Category = require('../../models/Category');
 
 // Criar categoria
 exports.createCategory = async (req, res) => {
--- src/routes/dashboards.js
+++ src/routes/dashboards.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const auth = require('../middleware/auth');
+const auth = require('../../middleware/auth');
 const dashboardController = require('../controllers/dashboardController');
 
 const router = express.Router();
