85215826da80ce2a4a504ac680b20af038c9182b
LisboaFred <contato.fredericolisboa@gmail.com>
2025-07-30 12:16:00 -0300

    feat: created settings and transaction

---
+++ Frontend/src/components/pages/Settings.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Settings() {
+  return <div className="p-6 text-lg">Página de Configurações (placeholder)</div>;
+}
---
+++ Frontend/src/components/pages/Transaction.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Transactions() {
+  return <div className="p-6 text-lg">Página de Transações (placeholder)</div>;
+}
